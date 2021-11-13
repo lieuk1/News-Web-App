f389d2c52795e65ca03d0a7932f57937c055317a
Kristella Lieu <lieuk1@tcnj.edu>
2021-11-12 21:40:42 -0500

    Refactor pagination and remove unneeded session variables

--- database.py
+++ database.py
@@ -6,15 +6,19 @@ db = SQLAlchemy()
 # ARTICLE DATABASE MODEL
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100))
     title = db.Column(db.String(500))
     description = db.Column(db.String(500))
     source = db.Column(db.String(100))
-    date = db.Column(db.String(20))
+    publish_date = db.Column(db.String(20))
     url = db.Column(db.String(500))
+    url_to_image = db.Column(db.String(500))
 
-    def __init__(self, title, description, source, date, url):
+    def __init__(self, author, title, description, source, publish_date, url, url_to_image):
+        self.author = author
         self.title = title
         self.description = description
         self.source = source
-        self.date = date
+        self.publish_date = publish_date
         self.url = url
+        self.url_to_image = url_to_image
